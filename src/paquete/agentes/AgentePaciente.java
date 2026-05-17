a6880e2c7ebe0c500a3b3687844127cbf2fb2ce2
alejandrogilrodr <alexgilcr9@gmail.com>
2026-05-17 21:22:57 +0200

    Create AgentePaciente.java

---
+++ src/paquete/agentes/AgentePaciente.java
@@ -0,0 +1,9 @@
+package paquete.agentes;
+
+import jade.core.Agent;
+
+public class AgentePaciente extends Agent {
+    protected void setup() {
+        System.out.println("Agente Paciente listo.");
+    }
+}
